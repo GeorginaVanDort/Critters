1eca7f43b41e5eb5a66a598392238394b6295682
Georgina Van Dort <georginavandort>
2016-10-01 18:46:18 -0700

    add readme file

---
+++ readme.md
@@ -0,0 +1,42 @@
+## Project Name
+Critter Tracker
+
+#### By Georgina Van Dort
+
+## Features
+This java app helps the employees of the Forest Service keep track of their animals.
+
+##Specifications
+
+1. User can add animals to an Animal database
+2. User can add Endangered Animals to the same database and include extra details
+3. User can track the health and age of endangered animals
+4. User can report sightings of animals that include location, user name and a Timestamp
+5. User can view a list of sightings by animal
+
+##This Project Includes
+* An example of an inherited class and a super class
+* Examples of constants to define Age and Health values
+* Illegal Argument Exceptions to catch illegal input from the User
+* Custom Error messages for Exceptions
+* Timestamps saved to the Database and displayed with correct formatting
+
+
+##Technologies
+Java, JUnit, Gradle, Spark, Velocity, PostgreSQL.
+
+## Setup/Installation Requirements
+* Please make sure Java, JRE, PostgreSQL and Gradle are installed.
+* Run PostgreSQL
+* In a second tab run psql and enter the following command:
+* $ CREATE DATABASE wildlife_tracker;
+*
+* In the original terminal window run the following:
+* $ psql  wildlife_tracker < wildlife_tracker.sql
+*
+* Then, clone this directory and execute "gradle run"
+* Navigate to localhost:4567
+
+
+## Support & Contact
+For questions, concerns, or suggestions please contact me on Github :)
--- wildlife_tracker.sql
+++ wildlife_tracker.sql
@@ -2,12 +2,17 @@
 -- PostgreSQL database dump
 --
 
+-- Dumped from database version 9.6rc1
+-- Dumped by pg_dump version 9.6rc1
+
 SET statement_timeout = 0;
 SET lock_timeout = 0;
+SET idle_in_transaction_session_timeout = 0;
 SET client_encoding = 'UTF8';
 SET standard_conforming_strings = on;
 SET check_function_bodies = false;
 SET client_min_messages = warning;
+SET row_security = off;
 
 --
 -- Name: plpgsql; Type: EXTENSION; Schema: -; Owner: 
@@ -30,7 +35,7 @@ SET default_tablespace = '';
 SET default_with_oids = false;
 
 --
--- Name: animals; Type: TABLE; Schema: public; Owner: Guest; Tablespace: 
+-- Name: animals; Type: TABLE; Schema: public; Owner: postgres
 --
 
 CREATE TABLE animals (
@@ -42,10 +47,10 @@ CREATE TABLE animals (
 );
 
 
-ALTER TABLE animals OWNER TO "Guest";
+ALTER TABLE animals OWNER TO postgres;
 
 --
--- Name: animals_id_seq; Type: SEQUENCE; Schema: public; Owner: Guest
+-- Name: animals_id_seq; Type: SEQUENCE; Schema: public; Owner: postgres
 --
 
 CREATE SEQUENCE animals_id_seq
@@ -56,17 +61,17 @@ CREATE SEQUENCE animals_id_seq
     CACHE 1;
 
 
-ALTER TABLE animals_id_seq OWNER TO "Guest";
+ALTER TABLE animals_id_seq OWNER TO postgres;
 
 --
--- Name: animals_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: Guest
+-- Name: animals_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: postgres
 --
 
 ALTER SEQUENCE animals_id_seq OWNED BY animals.id;
 
 
 --
--- Name: sightings; Type: TABLE; Schema: public; Owner: Guest; Tablespace: 
+-- Name: sightings; Type: TABLE; Schema: public; Owner: postgres
 --
 
 CREATE TABLE sightings (
@@ -78,10 +83,10 @@ CREATE TABLE sightings (
 );
 
 
-ALTER TABLE sightings OWNER TO "Guest";
+ALTER TABLE sightings OWNER TO postgres;
 
 --
--- Name: sightings_id_seq; Type: SEQUENCE; Schema: public; Owner: Guest
+-- Name: sightings_id_seq; Type: SEQUENCE; Schema: public; Owner: postgres
 --
 
 CREATE SEQUENCE sightings_id_seq
@@ -92,61 +97,68 @@ CREATE SEQUENCE sightings_id_seq
     CACHE 1;
 
 
-ALTER TABLE sightings_id_seq OWNER TO "Guest";
+ALTER TABLE sightings_id_seq OWNER TO postgres;
 
 --
--- Name: sightings_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: Guest
+-- Name: sightings_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: postgres
 --
 
 ALTER SEQUENCE sightings_id_seq OWNED BY sightings.id;
 
 
 --
--- Name: id; Type: DEFAULT; Schema: public; Owner: Guest
+-- Name: animals id; Type: DEFAULT; Schema: public; Owner: postgres
 --
 
 ALTER TABLE ONLY animals ALTER COLUMN id SET DEFAULT nextval('animals_id_seq'::regclass);
 
 
 --
--- Name: id; Type: DEFAULT; Schema: public; Owner: Guest
+-- Name: sightings id; Type: DEFAULT; Schema: public; Owner: postgres
 --
 
 ALTER TABLE ONLY sightings ALTER COLUMN id SET DEFAULT nextval('sightings_id_seq'::regclass);
 
 
 --
--- Data for Name: animals; Type: TABLE DATA; Schema: public; Owner: Guest
+-- Data for Name: animals; Type: TABLE DATA; Schema: public; Owner: postgres
 --
 
 COPY animals (id, name, health, endangered, age) FROM stdin;
+1	Furry Footed Chinchilla	\N	\N	\N
+2	Spiky Winged Bat	\N	\N	\N
+4	Secret Squirrel	$EndangeredAnimal.HEALTH_HIGH	t	$EndangeredAnimal.AGE_MED
 \.
 
 
 --
--- Name: animals_id_seq; Type: SEQUENCE SET; Schema: public; Owner: Guest
+-- Name: animals_id_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
 --
 
-SELECT pg_catalog.setval('animals_id_seq', 1, false);
+SELECT pg_catalog.setval('animals_id_seq', 4, true);
 
 
 --
--- Data for Name: sightings; Type: TABLE DATA; Schema: public; Owner: Guest
+-- Data for Name: sightings; Type: TABLE DATA; Schema: public; Owner: postgres
 --
 
 COPY sightings (id, rangername, location, animal_id, "time") FROM stdin;
+1	Ranger Dan	Up on the hill.	1	2016-10-01 15:52:27.244798
+2	Ranger Dan	Somewhere down near the river	2	2016-10-01 16:02:20.495948
+3	Ranger Dan	Somewhere down near the river	4	2016-10-01 17:08:44.156338
+4	Ranger Dan	Another Location	4	2016-10-01 17:28:55.414114
 \.
 
 
 --
--- Name: sightings_id_seq; Type: SEQUENCE SET; Schema: public; Owner: Guest
+-- Name: sightings_id_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
 --
 
-SELECT pg_catalog.setval('sightings_id_seq', 1, false);
+SELECT pg_catalog.setval('sightings_id_seq', 4, true);
 
 
 --
--- Name: animals_pkey; Type: CONSTRAINT; Schema: public; Owner: Guest; Tablespace: 
+-- Name: animals animals_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
 --
 
 ALTER TABLE ONLY animals
@@ -154,21 +166,11 @@ ALTER TABLE ONLY animals
 
 
 --
--- Name: sightings_pkey; Type: CONSTRAINT; Schema: public; Owner: Guest; Tablespace: 
+-- Name: sightings sightings_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
 --
 
 ALTER TABLE ONLY sightings
     ADD CONSTRAINT sightings_pkey PRIMARY KEY (id);
-
-
---
--- Name: public; Type: ACL; Schema: -; Owner: epicodus
---
-
-REVOKE ALL ON SCHEMA public FROM PUBLIC;
-REVOKE ALL ON SCHEMA public FROM epicodus;
-GRANT ALL ON SCHEMA public TO epicodus;
-GRANT ALL ON SCHEMA public TO PUBLIC;
 
 
 --
